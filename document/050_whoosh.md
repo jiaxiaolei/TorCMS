6029c3d3754cd3e46624b5d678f909820f32cf0f
bk <bk@g>
2015-08-11 20:14:13 +0800

    针对搜索功能，修改了 HTML 与样式。

---
+++ document/050_whoosh.md
@@ -0,0 +1,15 @@
+
+
+## 说明
+
+使用 脚本 script_gen_whoosh_database.py，生成数据库。
+然后在网站程序中，查询的时候使用即可。
+请自行编辑 script_gen_whoose_database.py文件，使更符合搜索习惯。
+
+### 注意
+
+Whoosh 的针对中文，使用结巴分词进行文本分析。
+
+### Warning
+
+The whoosh used here is for Chinese.
---
+++ torlite/core/tool/whoosh_tool.py
@@ -0,0 +1,32 @@
+from whoosh.fields import *
+from whoosh.index import create_in, open_dir
+from jieba.analyse import ChineseAnalyzer
+from whoosh.qparser import QueryParser
+
+analyzer = ChineseAnalyzer()
+schema = Schema(title=TEXT(stored=True, analyzer=analyzer), path=ID(stored=True),
+                content=TEXT(stored=True, analyzer=analyzer))
+# ix = config.ix
+# self.searcher = ix.searcher()
+ix = open_dir("lib/whoosh")
+parser = QueryParser("content", schema=ix.schema)
+
+
+def search(keyword, limit=20):
+    q = parser.parse(keyword)
+    try:
+        searcher1 = ix.searcher()
+        tt = searcher1.search(q, limit=limit)
+        return (tt)
+    finally:
+        searcher1.close()
+
+
+def search_pager(keyword, page_index=1, doc_per_page=10):
+    q = parser.parse(keyword)
+    try:
+        searcher1 = ix.searcher()
+        tt = searcher1.search(q, limit=page_index * doc_per_page)
+        return (tt[(page_index - 1) * doc_per_page: page_index * doc_per_page])
+    finally:
+        searcher1.close()
