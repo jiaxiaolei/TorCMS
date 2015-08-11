ceed9534fbb21482c3d47a1dc8507de8694c2248
bk <bk@g>
2015-08-11 20:06:15 +0800

    使用 whoosh，增加了全文检索的功能。 使用结巴分词，使 whoosh 支持中文。

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 '''
 Author: Bu Kun
-E-mail: bukun@osgeo.cn
-CopyRight: http://yunsuan.org
+E-mail: bukun#osgeo.cn
+CopyRight: http://www.yunsuan.org
 '''
 
 import tornado.web
